aa8b9a0340f52db37092c8d381179072dd9f529d
Calebe Rodrigues <calebebrodrigues@gmail.com>
2021-06-12 12:56:46 -0300

    Busca por CEP - finalizado

--- js/main.js
+++ js/main.js
@@ -5,12 +5,32 @@ if (consulta) consulta.addEventListener('click', (e) => {
 });
 
 function consultaCep() {
+    const cep = document.querySelector('#cep').value;
+
     $.ajax({
-        url: 'https://viacep.com.br/ws/14781449/json/',
+        url: `https://viacep.com.br/ws/${cep}/json/`,
         type: 'GET',
         success: function (response) {
-            console.log(response);
-            alert(`A rua selecionada foi: ${response.logradouro} no Bairro: ${response.bairro}`);
+            exibeInformacoes(response);
         }
     })
+}
+
+function exibeInformacoes(resp) {
+    const html = `
+        <br>
+
+        <center><h2>CEP: ${resp.cep}</h2></center>
+
+        <center>
+            <p>
+                Logradouro: ${resp.logradouro} <br>
+                Bairro: ${resp.bairro} <br>
+                Localidade: ${resp.localidade} <br>
+                UF: ${resp.uf} <br>
+                DDD: ${resp.ddd}
+            </p>
+        </center>
+    `;
+    $('.main').html(html);
 }
